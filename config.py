7c795030e2550175f6919ffa83fc63c6e06c0fb8
dhpl-developers <dhpl.developers@gmail.com>
2025-06-27 14:41:50 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    CLIENT_ID = "654156985064-vt48t8gj3qod98m4toivp6975lcdojom.apps.googleusercontent.com"
+    CLIENT_SECRET = "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"
+    REDIRECT_URI = "https://dhanashrichatbot-xnkxs2akkodv7t5kzhrumh.streamlit.app"
