8fbc4b7b4c2cf9e344ee6008e13cc4beb0947f5b
dhpl-developers <dhpl.developers@gmail.com>
2025-06-28 15:27:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,14 @@
 class Config:
+    # Google OAuth config
     CLIENT_ID = "654156985064-vt48t8gj3qod98m4toivp6975lcdojom.apps.googleusercontent.com"
     CLIENT_SECRET = "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"
     REDIRECT_URI = "https://dhanashrichatbot-xnkxs2akkodv7t5kzhrumh.streamlit.app"
+
+    # Azure Blob Storage config
+    AZURE_CONNECTION_STRING = (
+    "DefaultEndpointsProtocol=https;"
+    "AccountName=imagestoragedata;"
+    "AccountKey=+2CxrTBgIqQIotfoxH/Af52lK/0qrWma2hSgSZDySzJ9rnRdOnd4cMq1USeBAWVtZQYzy3jKPAoh+ASt09QcOQ==;"
+    "EndpointSuffix=core.windows.net"
+)
+
